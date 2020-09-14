3193e9f8c36132ddead3bff6c9f75fd0ccf487a1
Gabriel Nogueira <gn@hellofresh.com>
2020-09-14 17:01:28 +0200

    persist clipping on database

--- app/renderer.js
+++ app/renderer.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 
 import { clipboard, ipcRenderer } from 'electron';
 
+import database from './database';
+
 const writeToClipboard = (content) => {
   clipboard.writeText(content);
 };
@@ -20,25 +22,27 @@ class Application extends React.Component {
     };
 
     this.addClipping = this.addClipping.bind(this);
+    this.fetchClippings = this.fetchClippings.bind(this);
     this.handleWriteToClipboard = this.handleWriteToClipboard.bind(this);
   }
 
   componentDidMount() {
     ipcRenderer.on('create-new-clipping', this.addClipping);
     ipcRenderer.on('write-to-clipboard', this.handleWriteToClipboard);
+
+    this.fetchClippings();
+  }
+
+  fetchClippings() {
+    database('clippings')
+      .select()
+      .then((clippings) => this.setState({ clippings }));
   }
 
   addClipping() {
-    const { clippings } = this.state;
-
     const content = clipboard.readText();
-    const id = Date.now();
 
-    const clipping = { id, content };
-
-    this.setState({
-      clippings: [clipping, ...clippings],
-    });
+    database('clippings').insert({ content }).then(this.fetchClippings);
   }
 
   handleWriteToClipboard() {
@@ -67,20 +71,18 @@ class Application extends React.Component {
   }
 }
 
-const Clipping = ({ content }) => {
-  return (
-    <article className="clippings-list-item">
-      <div className="clipping-text" disabled>
-        {content}
-      </div>
-      <div className="clipping-controls">
-        <button onClick={() => writeToClipboard(content)}>
-          &rarr; Clipboard
-        </button>
-        <button>Update</button>
-      </div>
-    </article>
-  );
-};
+const Clipping = ({ content }) => (
+  <article className="clippings-list-item">
+    <div className="clipping-text" disabled>
+      {content}
+    </div>
+    <div className="clipping-controls">
+      <button onClick={() => writeToClipboard(content)}>
+        &rarr; Clipboard
+      </button>
+      <button>Update</button>
+    </div>
+  </article>
+);
 
 render(<Application />, document.getElementById('application'));
